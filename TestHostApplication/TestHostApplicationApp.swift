f4987a9e1fb4f44edf58240d0b5bda1c4a7c1880
Gaétan Zanella <gaetan.zanella@fabernovel.com>
2021-04-15 17:26:15 +0200

    Create TestHostApplicationApp

---
+++ TestHostApplication/TestHostApplicationApp.swift
@@ -0,0 +1,18 @@
+//
+//  TestHostApplicationApp.swift
+//  TestHostApplication
+//
+//  Created by Gaétan Zanella on 15/04/2021.
+//  Copyright © 2021 Fabernovel. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct TestHostApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
